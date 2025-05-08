0a03b0ae515a51c1f3672d024b89592d8fea6584
iamkunalb <kbha192@aucklanduni.ac.nz>
2025-05-08 23:29:41 +1000

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -1,7 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("./amoria.json")
+cred = credentials.Certificate("/secrets/firebase.json")
 firebase_admin.initialize_app(cred)
+
+# cred = credentials.Certificate("./amoria.json")
+# firebase_admin.initialize_app(cred)
 
 db = firestore.client()
