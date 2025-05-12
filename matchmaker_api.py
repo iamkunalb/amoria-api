f4a64bc4ec6c2b65e42cde8b8fd69df8b7b19fd1
iamkunalb <kbha192@aucklanduni.ac.nz>
2025-05-12 20:02:04 +1000

    Update matchmaker_api.py

--- matchmaker_api.py
+++ matchmaker_api.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://amoria-one.vercel.app"],
+    allow_origins=["https://amoria-one.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
