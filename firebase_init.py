2583aa3348b1e11ae9bc7a8c014310c148ab928b
iamkunalb <kbha192@aucklanduni.ac.nz>
2025-05-22 02:09:11 +1000

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -1,7 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# ✅ Use Cloud Run's built-in identity
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
 
