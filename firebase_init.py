78dbe06ed517deafd22d54a6c98c5410c2bda3df
iamkunalb <kbha192@aucklanduni.ac.nz>
2025-05-22 15:19:48 +1000

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # cred = credentials.Certificate("/secrets/firebase.json")
-cred = credentials.Certificate("/secrets/firebase-service-key")
+cred = credentials.Certificate("/secrets/secrets/firebase-service-key")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
